c7e40d71c4cc8924b0e25d0839c98254581f0392
Santiago Gil <santiago.gil@davinci.edu.ar>
2025-11-24 15:54:18 -0300

    modificacion de .sql

--- musynf.sql
+++ musynf.sql
@@ -1,22 +1,31 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-11-2025 a las 19:37:16
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- /*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Base de datos: `musync`
-CREATE DATABASE IF NOT EXISTS `musync` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `musync`;
+--
+-- Base de datos: `musynf`
+--
 
 -- --------------------------------------------------------
--- Tabla `artista`
--- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artista`
+--
 
 CREATE TABLE `artista` (
   `Id` int(11) NOT NULL,
@@ -34,6 +43,10 @@ CREATE TABLE `artista` (
   `Imagen_Not` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `artista`
+--
+
 INSERT INTO `artista` (`Id`, `Inicio_Actividad`, `Fecha_Nacimiento`, `Nombre`, `Apellido`, `Nombre_Artistico`, `Nombre_Artistico_Anterior`, `Nacionalidad`, `Fin_Actividad`, `Imagen`, `Descripcion`, `Noticia`, `Imagen_Not`) VALUES
 (1, '2015-03-25', '1997-10-10', 'Valentin Oliva', NULL, 'Wos', NULL, 'Argentino', NULL, './Img/Fotos_WOS.pdf.jpg', 'Wos es un rapero, cantante y freestyler argentino reconocido por su potencia vocal, letras introspectivas y compromiso social. Surgió en las batallas de freestyle, ganó la Red Bull Internacional y luego consolidó una exitosa carrera musical con discos que mezclan rap, rock y música alternativa.', 'Wos anunció nuevas fechas para su gira internacional, sumando presentaciones en España y México tras el éxito de su último álbum.', './Img/wos_noti.jpg'),
 (2, '2000-01-25', '1998-05-08', 'Alejo Nahuel Acosta Migliarini', NULL, 'Ysy A', NULL, 'Argentino', NULL, './Img/ysy_foto.jpg', 'Ysy A es un rapero y compositor argentino, fundador del icónico colectivo Modo Diablo. Con un estilo innovador y energético, mezcla trap, rap y electrónica. Es conocido por su creatividad, sus conceptos artísticos y por lanzar música cada 11/11 como parte de su identidad artística.', 'Ysy A confirmó que el próximo 11/11 lanzará un nuevo proyecto conceptual, manteniendo su tradición anual de estrenos en esa fecha.', './Img/ysy_noti.jpg'),
@@ -49,8 +62,49 @@ INSERT INTO `artista` (`Id`, `Inicio_Actividad`, `Fecha_Nacimiento`, `Nombre`, `
 (12, NULL, NULL, '', NULL, 'Trueno', NULL, NULL, NULL, './Img/trueno.jpeg', 'Trueno es un rapero y freestyler argentino que combina rap, trap y ritmos latinos. Ganó reconocimiento por su talento en batallas y luego por su música cargada de identidad, mensaje social y energía. Es una de las voces más representativas de la nueva generación urbana.', 'Trueno anunció una gira por Latinoamérica con un show renovado que mezcla rap, folclore y ritmos afro-latinos.', './Img/trueno_noti.jpg');
 
 -- --------------------------------------------------------
--- Tabla `usuario`
+
+--
+-- Estructura de tabla para la tabla `configuracion_sitio`
+--
+
+CREATE TABLE `header` (
+  `Id` int(11) NOT NULL,
+  `Nombre_Sitio` varchar(100) NOT NULL,
+  `Logo` varchar(200) DEFAULT NULL,
+  `Color_Primario` varchar(20) DEFAULT '#1abc54'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `configuracion_sitio`
+--
+
+INSERT INTO `header` (`Id`, `Nombre_Sitio`, `Logo`, `Color_Primario`) VALUES
+(1, 'Musynf', './Img/spotify_black.png', '#1abc54');
+
 -- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `footer_info`
+--
+
+CREATE TABLE `footer_info` (
+  `Id` int(11) NOT NULL,
+  `Texto` varchar(300) DEFAULT NULL,
+  `Email` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `footer_info`
+--
+
+INSERT INTO `footer_info` (`Id`, `Texto`, `Email`) VALUES
+(1, 'Musynf © 2025 - Proyecto académico de música urbana.', 'contacto@musynf.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
 
 CREATE TABLE `usuario` (
   `Id` int(11) NOT NULL,
@@ -63,28 +117,73 @@ CREATE TABLE `usuario` (
   `Correo` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
 INSERT INTO `usuario` (`Id`, `Fecha_Alta`, `Fecha_Nacimiento`, `Nombre`, `Apellido`, `Nombre_Usuario`, `Contrasenia`, `Correo`) VALUES
 (1, '2025-10-05', '2002-10-27', 'Tomas', 'Fernandez', 'tomifer', 'futbol123', 'tomi@gmail.com'),
 (2, '2025-07-15', '1993-07-27', 'Joaquin', 'Dalmau', 'joadalmau', 'trap2025', 'joaco@gmail.com');
 
--- Índices
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `artista`
+--
 ALTER TABLE `artista`
   ADD PRIMARY KEY (`Id`);
 
+--
+-- Indices de la tabla `configuracion_sitio`
+--
+ALTER TABLE `header`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `footer_info`
+--
+ALTER TABLE `footer_info`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `usuario`
+--
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`Id`),
   ADD UNIQUE KEY `uq_usuario_nombre_usuario` (`Nombre_Usuario`),
   ADD UNIQUE KEY `uq_usuario_correo` (`Correo`);
 
--- AUTO_INCREMENT
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `artista`
+--
 ALTER TABLE `artista`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
+--
+-- AUTO_INCREMENT de la tabla `configuracion_sitio`
+--
+ALTER TABLE `header`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `footer_info`
+--
+ALTER TABLE `footer_info`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
 ALTER TABLE `usuario`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
- /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
- /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
